da01fe2bd178716386c71599093f60c0a8526901
vkhauv <v.khauv@me.com>
2014-12-05 18:18:19 -0800

    Update request.py

--- FinalProject/request.py
+++ FinalProject/request.py
@@ -1,19 +1,20 @@
 import requests
 
-idconverter = {}
-ids = open("snp_omim.txt","r")
-for line in ids:
-    line = line.rstrip('\n')
-    currPair = line.split(" ")
-    idconverter[currPair[0]] = currPair[1]
-ids.close()
+#idconverter = {}
+#ids = open("snp_omim.txt","r")
+#for line in ids:
+#    line = line.rstrip('\n')
+#    currPair = line.split(" ")
+#    idconverter[currPair[0]] = currPair[1]
+#ids.close()
+#mim = list(idconverter.values())  
 
-print idconverter
-
-mim = list(idconverter.values())  
+matches = open("matchlist.txt", "r")
+mim = matches.read()
+mim = mim.split('\n')
 
 print mim
-
+print len(mim)
 for x in range(len(mim)):
 	mimNumber = mim[x]
 	url = "http://api.omim.org/api/entry?apiKey=2D38BBD07E6389672AE6469A244285FA6C7209F2&include=all&mimNumber=" + mimNumber
