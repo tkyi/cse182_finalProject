961dbb7ae6ab87341840c7db9d3f7471eca89157
vkhauv <v.khauv@me.com>
2014-12-12 23:02:10 -0800

    updated new version, added comments

--- FinalProject/match.py
+++ FinalProject/match.py
@@ -1,33 +1,56 @@
 #!/usr/bin/python
 
-num = []
-snpnum = []
+num = [] #contains all rsids
+snpnum = [] #contains all rsids that match from omimdb
+omimmatch = [] #omimids corresponding 
+
+#gets all rs numbers for particular individual
 vcf = open("individualX.txt", "r") #opens file for reading
 for line in vcf:
 	data = line.split( )
 	if data[2] is not '.':
-		new = data[2]
-		new = new[2:]
+		new = data[2] #rsid
+		new = new[2:] #removes rs
 		num.append(new)
 
+#gets all rs numbers from omimdb
 snp = open("snp_omim.txt", "r")
 for line in snp:
 	data = line.split( )
-	new = data[1]
-	new = new[0:]
-	snpnum.append(new)
+	new1 = data[0]
+	snpnum.append(new1)
+snp.close()
 
+#gets matches for rsids
+matches =  set(num) & set(snpnum) 
+#turns into list
+match = list(matches)
 
-matches =  set(num) & set(snpnum)
-print matches
+#gets all matches and gets the omim numbers to store into omimmatch
+snp = open("snp_omim.txt", "r")
+for line in snp:
+	print line
+	data = line.split( )
+	print data[0]
+	for j in range(len(match)):
+		if match[j] == data[0]:
+			omimmatch.append(data[1]) #append data
+				
+#print matches
+print snpnum
+print match
+print omimmatch
 
-print len(matches)
-print len(snpnum)
-print len(num)
-
+#create matchlist of rs ids
 matchlist = 'matchlist.txt'
 with open(matchlist, 'w') as fout:
 	for id in matches:
   		fout.write(id)
   		fout.write("\n")
-	
+
+#create matchlist of omim ids
+omim = 'omim.txt'
+with open(omim, 'w') as omimout:
+	for k in omimmatch:
+  		omimout.write(k)
+  		omimout.write("\n")
